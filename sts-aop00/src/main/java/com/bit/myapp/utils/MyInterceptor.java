ce3c8bb8f863661c2b9ca18ae7b53c6f10bf8951
bit05class <bit05class@gmail.com>
2019-01-15 17:47:25 +0900

    step2 - interceptor

---
+++ sts-aop00/src/main/java/com/bit/myapp/utils/MyInterceptor.java
@@ -0,0 +1,38 @@
+package com.bit.myapp.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
+
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		logger.debug("controller before ...");
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		logger.debug("controller after ...");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+		logger.debug("view rander ...");
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+}
